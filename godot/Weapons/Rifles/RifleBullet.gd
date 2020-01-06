84bdc6cefc2239bf700bc1adc17c5775a6c0b7ad
Laletin Dmitry <laletindn@mail.ru>
2020-01-06 13:11:52 +0400

    implement custom map
    Former-commit-id: 25fe2bf5e9062026bd7228a97b4a6e25b4a9779e [formerly 970239de89a8fb7a9900f058876aa4cc582d841e]
    Former-commit-id: aae4a7f818aa725264eb23a525f400cb77156228

--- godot/Weapons/Rifles/RifleBullet.gd
+++ godot/Weapons/Rifles/RifleBullet.gd
@@ -13,6 +13,7 @@ func _physics_process(delta):
 	if collision:
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
+		queue_free()
 
 func _on_Lifetime_timeout():
 	queue_free()
