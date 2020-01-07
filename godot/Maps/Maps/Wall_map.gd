8fe8562e6a2dae3c8fb5c127feb8da2b198a63d2
Freezee <apodolyak.99@mail.ru>
2020-01-07 13:42:59 +0700

    Add doors and windows

--- godot/Maps/Maps/Wall_map.gd
+++ godot/Maps/Maps/Wall_map.gd
@@ -23,14 +23,24 @@ func _on_Area2D_body_entered(body):
 	print(body.position)
 	print("Entered")
 	var pos = man.position
-	var x = int(pos.x/64)
-	var y = int(pos.y/64)
-	if x in range(-1, 1):
-		x = -1
-	if y in range(-1, 1):
-		y = -1
+	var x
+	var y
+	if pos.x < 0:
+		x = int(pos.x/64) - 1
+	else:
+		x = int(pos.x/64)
+	if pos.y < 0:
+		y = int(pos.y/64) - 1
+	else:
+		y = int(pos.y/64)
 	print(x, " ", y)
-	cell = get_cell(x, y)
+	cell = get_cell(x+1, y)
+	if cell == -1:
+		cell = get_cell(x, y+1)
+		if cell == -1:
+			cell = get_cell(x-1, y)
+			if cell == -1:
+				cell = get_cell(x, y-1)
 	var ar = get_used_cells()
 	print("Cell_id? ", cell)
 	print("Cells: ", ar)
