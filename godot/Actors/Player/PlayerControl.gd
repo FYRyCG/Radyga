7bbb166d6c75b37f7553c84ed4c6efce2ebb5d1f
Vyacheslav <ytwog@mail.ru>
2020-01-09 01:17:40 +0700

    fix init
    Former-commit-id: f125082cd5433d0bcf56f7b93f79810881a4a70d

--- godot/Actors/Player/PlayerControl.gd
+++ godot/Actors/Player/PlayerControl.gd
@@ -13,7 +13,25 @@ func _start(player_):
 	player = weakref(player_)
 
 func _physics_process(delta):
-	player.get_ref().look_at(get_global_mouse_position())
+	# Плавный поворот player'a + отключение лишнего движения при поворотах
+	var angle_dif_by_mouse = player.get_ref().get_angle_to(get_global_mouse_position())
+	var rt = 5.0 # rotation_threshold
+	if(angle_dif_by_mouse != 0):
+		var new_rotation = player.get_ref().rotation_degrees + angle_dif_by_mouse*10.0
+		var collision = player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(rt * 1.0,0.0)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(rt * 1.0,rt * 1.0)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(-1.0 * rt,0.0)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(-1.0 * rt,1.0 * rt)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(0.0,1.0 * rt)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(0.0,-1.0 * rt)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(-1.0 * rt,-1.0 * rt)) || player.get_ref().test_move( Transform2D (new_rotation, player.get_ref().position),
+		 Vector2(1.0 * rt,-1.0 * rt))
+		print(collision)
+		if(!collision):
+			player.get_ref().rotation_degrees = new_rotation
+	# Старый вариант:
+	#player.get_ref().look_at(get_global_mouse_position())
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
@@ -39,6 +57,4 @@ func _physics_process(delta):
 		player.get_ref().use()
 		
 	velocity = velocity.normalized() * player_speed
-
 	velocity = player.get_ref().move_and_slide(velocity)
-	
