c9d8fb80b4bba1010e16babe9c8327d2c2222e56
Freezee <apodolyak.99@mail.ru>
2020-01-09 17:27:02 +0700

    Add player_vision
    Former-commit-id: 4a07c5a95e6a5b28c809daf42dd438eecb87333d

--- godot/Maps/Maps/Wall_map.gd
+++ godot/Maps/Maps/Wall_map.gd
@@ -10,30 +10,9 @@ func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var pos = man.position
-	var x
-	var y
-	if pos.x < 0:
-		x = int(pos.x/64) - 1
-	else:
-		x = int(pos.x/64)
-	if pos.y < 0:
-		y = int(pos.y/64) - 1
-	else:
-		y = int(pos.y/64)
-	print(x, " ", y)
-	cell = get_cell(x+1, y)
-	if cell == -1:
-		cell = get_cell(x, y+1)
-		if cell == -1:
-			cell = get_cell(x-1, y)
-			if cell == -1:
-				cell = get_cell(x, y-1)
-	var ar = get_used_cells()
-	print("Cell_id ", cell)
-
+	pass
+"""
 func _on_Area2D_body_entered(body):
-
 	man = body
 	current_room = $Room_1
 	print(man.name, "Entered ", current_room.name)
@@ -54,4 +33,4 @@ func _on_Room_2_body_entered(body):
 func _on_Room_2_body_exited(body):
 	print(man.name, "Exited ", $Room_2.name)
 	emit_signal("room_exited", $Room_2)
-
+"""
