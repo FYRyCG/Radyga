05c89e76cbeaf78f9bafc72878b791786c94f34e
Freezee <apodolyak.99@mail.ru>
2020-01-09 14:53:49 +0700

    Fix Visibility of rooms
    Former-commit-id: 0e30f5ae2e1a006383ce12a24339fc6279cff0ef [formerly 055f2bfcc57cfec9d8a96aaf8a7614526bb3348e]
    Former-commit-id: 20014c5c122993cdc1dff6cbd12d4531ce35f913

--- godot/Maps/Maps/Wall_map.gd
+++ godot/Maps/Maps/Wall_map.gd
@@ -5,7 +5,7 @@ var man
 var cell
 var current_room
 signal room_changed(current_room)
-
+signal room_exited(current_room)
 func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,7 +21,7 @@ func _process(delta):
 		y = int(pos.y/64) - 1
 	else:
 		y = int(pos.y/64)
-	#print(x, " ", y)
+	print(x, " ", y)
 	cell = get_cell(x+1, y)
 	if cell == -1:
 		cell = get_cell(x, y+1)
@@ -30,16 +30,26 @@ func _process(delta):
 			if cell == -1:
 				cell = get_cell(x, y-1)
 	var ar = get_used_cells()
-	#print("Cell_id ", cell)
+	print("Cell_id ", cell)
 
 func _on_Area2D_body_entered(body):
-	print("Entered")
 	man = body
+	current_room = $Room_1
+	print(man.name, "Entered ", current_room.name)
+	
+	emit_signal("room_changed", current_room)
 	
 func _on_Area2D_body_exited(body):
-	print("Exited")
-	
-func _on_Room_1_area_entered(area):
-	print("Ent")
-	current_room = area
-	emit_signal("room_changed")
+	print(man.name, "Exited ", $Room_1.name)
+	emit_signal("room_exited", $Room_1)
+
+func _on_Room_2_body_entered(body):
+	man = body
+	current_room = $Room_2
+	print(man.name, "Entered ", current_room.name)
+	emit_signal("room_changed", current_room)
+
+
+func _on_Room_2_body_exited(body):
+	print(man.name, "Exited ", $Room_2.name)
+	emit_signal("room_exited", $Room_2)
