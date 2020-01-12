8c9d5b4c1085cb29f941c712f072c4f6b7c3e3c5
Dmitry Laletin <laletindn@mail.ru>
2020-01-12 23:24:27 +0300

    improve smokes
    Former-commit-id: bcc4ae97aaaf6e0a6234dc4b6df25dba172cd787

---
+++ godot/Actors/Player/PlayerElements.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _on_VisionArea_body_entered(body):
+	if body.has_method("vision_entered"):
+		body.call("vision_entered")
+
+
+func _on_VisionArea_body_exited(body):
+	if body.has_method("vision_exited"):
+		body.call("vision_exited")
