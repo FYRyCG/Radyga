ec84dc7e1696cc55725bd69693911de6acdb131a
Dmitry Laletin <laletindn@mail.ru>
2020-01-12 04:28:46 +0300

    add base grenade
    Former-commit-id: eed09682f6f98543a400c4f299c1ff18ab6d67a7

--- godot/Equipments/Grenades/FragGrenade/FragGrenade.gd
+++ godot/Equipments/Grenades/FragGrenade/FragGrenade.gd
@@ -1,71 +1,31 @@
 extends KinematicBody2D
 
 export (int) var speed = 300 setget set_speed, get_speed
-export (int) var damage = 67 setget set_damage, get_damage
-
-var remain_speed_factor = 1.0  # maybe
-var stop_force = 0.6 # начальная сила зависит от оперативника, и силы броска
-
-var velocity = Vector2()
-var stop_velovity = Vector2()
-
-var throws = false
-
-func _ready():
-	set_physics_process(false)
-	set_process(false)
+export (int) var damage = 67
+export (int) var explosion_time = 2
 
 func start():
-	set_physics_process(false)
-	set_process(false)
-	$ExplosionTimer.start()
+	$GrenadeBase.start(explosion_time)
 
 func throw():
-	var pl = get_parent()
-	pl.remove_child(self)
-	global_position = pl.get_global_position()
-	global_rotation = pl.get_global_rotation()
-	pl.get_parent().add_child(self)
-	
-	velocity = Vector2(speed, 0).rotated(rotation)
-	stop_velovity = Vector2(stop_force, 0).rotated(rotation)
-	set_physics_process(true)
-
-func cancel():
-	queue_free()
-
-func _physics_process(delta):
-	#print("process ")
-	var collision = move_and_collide(velocity * delta * remain_speed_factor)
-	if collision:
-		velocity = velocity.bounce(collision.normal)
-		stop_force += 0.4
-		
-	if remain_speed_factor != 0:	
-		remain_speed_factor -= stop_force * delta
-		if remain_speed_factor < 0:
-			remain_speed_factor = 0
-
+	$GrenadeBase.throw(speed)
 
 var explosion_thread = Thread.new()
-func _on_ExplosionTimer_timeout():
-	set_physics_process(false)
-	remain_speed_factor = 0  # fix bug with throwing explosion
-	
-	$Sprite.hide()
-	$ExplosionSprites.show()
-	$ExplosionSprites.play("explosion")
-	
-	explosion_thread.start(self, "explosion", null)
+func explosion():
+	explosion_thread.start(self, "_bg_explosion", null)
 
-func explosion(args):
+func _bg_explosion(args):
+	$Sprite.call("hide")
+	$ExplosionSprites.call("show")
+	$ExplosionSprites.call("play", "explosion")
+	
 	for inc in range(11):
 		call_deferred("set_damage", damage / 10.0 * inc)
 		$ExplosionArea.set_deferred("scale", Vector2(0.1, 0.1) * inc)
 		OS.delay_msec(15)
-	call_deferred("exploded")
+	call_deferred("_exploded")
 
-func exploded():
+func _exploded():
 	explosion_thread.wait_to_finish()
 	queue_free()
 
@@ -78,10 +38,4 @@ func set_speed(new_speed):
 
 func get_speed():
 	return speed
-
-func set_damage(new_damage):
-	damage = new_damage
-	
-func get_damage():
-	return damage
 
