dfc496d5f8150aecb93c2c23e5101c7478b90b93
Vyacheslav <ytwog@mail.ru>
2020-01-23 20:09:44 +0700

    AnimationPlayer within Weapon's node M4
    Former-commit-id: e59f008fbea431e75807d0cb68a0823579023a34

--- godot/Actors/Player/Player.gd
+++ godot/Actors/Player/Player.gd
@@ -157,7 +157,6 @@ func start_animation(velocity):
 			else:
 				blend = demanded_blend
 				equipped_animation = new_equipped
-			print(blend)
 			get_node("AnimationPlayer").get_node("AnimationTree")["parameters/Idle/blend_position"] = blend
 			get_node("AnimationPlayer").get_node("AnimationTree")["parameters/Walk/blend_position"] = blend
 			get_node("AnimationPlayer").get_node("AnimationTree")["parameters/Use/blend_position"] = blend
