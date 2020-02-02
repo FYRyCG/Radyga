c1c1077f53aaacdcc01b204b15ee65c2fcd7bc05
Laletin Dmitry <LaletinDN@mail.ru>
2020-02-02 14:21:45 +0300

    implement HUD for ammo
    Former-commit-id: bf45d06f393d38430a58e9c6c336e489682be5d1

--- godot/Actors/Interface/HUD/Weapons_UI/Weapons.gd
+++ godot/Actors/Interface/HUD/Weapons_UI/Weapons.gd
@@ -1,59 +1,76 @@
 extends VBoxContainer
 
-
 onready var bullet_conter = $HBoxContainer/Bullets/Bullets_counter
 var curent_ammo
 var curent_mags
-var curent_primary_weapon = "res://Actors/Interface/HUD/Weapons_UI/Main_weap.png"   #Текстура
-var curent_secondary_weapon  = "res://Actors/Interface/HUD/Weapons_UI/Sec_weap.png"  #Текстура
-onready var curent_weapon = $Primary   #Оружие в руках (Prim/Sec)
-var weapon_compliance = {1: $Primary, 2: $Secondary}
 
+var curent_primary_weapon_texture = "res://Actors/Interface/HUD/Weapons_UI/Main_weap.png"   #Текстура
+var curent_secondary_weapon_texture  = "res://Actors/Interface/HUD/Weapons_UI/Sec_weap.png"  #Текстура
+
+onready var curent_weapon_texture = $Primary   #Оружие в руках (Prim/Sec)
+onready var weapon_compliance = {1: $Primary, 2: $Secondary}
+
+var curent_weapon
 
 func _ready():
 	pass
 	
-	
-func initilize(max_ammo, max_mags):
-	curent_ammo = max_ammo
-	curent_mags = max_mags
-	$HBoxContainer/Bullets/Bullets_counter.text = str(curent_ammo) + " / " + str(curent_mags)
-	$Primary.texture = load(curent_primary_weapon)
-	$Secondary.texture = load(curent_secondary_weapon)
-	curent_weapon.visible = true
-	
-	
+func initilize(primary_weapon, secondary_weapon):
+	pass
+
+func show_weapon(weapon, extra_ammo):
+	if curent_weapon and curent_weapon.get_ref():
+		curent_weapon.get_ref().disconnect("shoot", self, "_ammo_changed")
+		
+	curent_ammo = weapon.ammo
+	curent_mags = extra_ammo
+	curent_weapon = weakref(weapon)
+	weapon.connect("shoot", self, "_ammo_changed")
+	set_bullet_counter(curent_ammo, curent_mags)
+	set_weapon_loadout(weapon)
+	show_curent_weapon(1 if weapon.get_type() == "primary" else 2)
+
 func set_bullet_counter(ammo, mags):
 	bullet_conter.text = str(round(ammo)) + " / " + str(round(mags))
 
+func update_bullet_counter():
+	bullet_conter.text = str(round(curent_ammo)) + " / " + str(round(curent_mags))
 
 func animate_value(curent_ammo, curent_mags, ammo, mags):
 	pass
-	
-	
+
 func _on_HUD_ammo_changed(ammo, mags):
 	#animate_value(curent_ammo, curent_mags, ammo, mags)
 	set_bullet_counter(ammo, mags)
 	curent_ammo = ammo
 	curent_mags = mags
 
+func _ammo_changed(ammo):
+	curent_ammo = ammo
+	update_bullet_counter()
 
-func set_weapon_loadout(primary, secondary):  #При подборе оружия
-	$Primary.texture = primary
-	$Secondary.texture = secondary
-	curent_primary_weapon = primary
-	curent_secondary_weapon = secondary
+func _extra_ammo_changed(extra_ammo):
+	curent_ammo = extra_ammo
+	update_bullet_counter()
+
+func set_weapon_loadout(weapon):  #При подборе оружия
+	if weapon.get_type() == "primary":
+		curent_primary_weapon_texture = weapon.get_node("Sprite").texture
+		$Primary.texture = weapon.get_node("Sprite").texture
+	else:
+		curent_secondary_weapon_texture = weapon.get_node("Sprite").texture
+		$Secondary.texture = weapon.get_node("Sprite").texture
 
 
-func show_curent_weapon (number): #При переключении оружий 
+func show_curent_weapon(number): #При переключении оружий 
 	#number - номер клавиши (1 или 2) 
-	curent_weapon.visible = false
-	curent_weapon = weapon_compliance[number]
-	curent_weapon.visible = true
+	curent_weapon_texture.visible = false
+	curent_weapon_texture = weapon_compliance[number]
+	curent_weapon_texture.visible = true
 
 
-func _on_HUD_set_loadout(primary, secondary):
-	set_weapon_loadout(primary, secondary)
+func _on_HUD_set_loadout(weapon):
+	set_weapon_loadout(weapon)
 
 
 func _on_HUD_weapon_swap(number):
