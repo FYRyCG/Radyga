20866a28ee4524f263caded653fa3e2fed8b76cd
Freezee <apodolyak.99@mail.ru>
2020-02-04 21:18:04 +0700

    Add main buttons
    Former-commit-id: f8aaf9ec51cd7e997d6ce8e68b9e327b3a07dc00

--- godot/World/Main_menu.gd
+++ godot/World/Main_menu.gd
@@ -12,21 +12,34 @@ func _on_Button_pressed():
 
 
 func _on_Select_level_pressed():
-	$Level_panel.visible = true
+	pass
 
 
 
 func _on_Start_pressed():
+	$Level_panel.visible = true
+	"""
 	get_tree().get_root().add_child(lobby.instance())
 	self.queue_free()
-
+	"""
 
 func _on_Simple_Button_pressed():
 	MapManager.load_map("res://Maps/Maps/SimpleMap.tscn")
 	$Level_panel.visible = false
+	get_tree().get_root().add_child(lobby.instance())
+	self.queue_free()
 
 
 func _on_Admin_Button_pressed():
 	MapManager.load_map("res://Maps/Maps/Admin_building/Admin_building.tscn")
 	$Level_panel.visible = false
+	get_tree().get_root().add_child(lobby.instance())
+	self.queue_free()
 
+
+func _on_Button1_pressed(op_name):
+	print(op_name)
+
+
+func _on_Operators_pressed():
+	$Operators.show()
