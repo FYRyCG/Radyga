e16c58d125d5ce809ebcedc69b46c040329f30bb
Vyacheslav <ytwog@mail.ru>
2020-11-02 21:07:37 +0700

    Floor trigger fix

--- godot/Cameras/Floor_levels_Detection.gd
+++ godot/Cameras/Floor_levels_Detection.gd
@@ -5,13 +5,15 @@ func _ready():
 
 
 func _on_Floor_area_entered(area, floor_id):
-	print(area.name, "Entered")
-	var player_cam = area.get_parent().get_node("Camera")
-	player_cam.set_cull_mask(floor_id)
+	if area.get_parent().has_node("Camera"):
+		print(area.name, "Entered")
+		var player_cam = area.get_parent().get_node("Camera")
+		player_cam.set_cull_mask(floor_id)
 
 
 
 func _on_Floor_area_exited(area, floor_id):
-	print(area.name, "Exited")
-	var player_cam = area.get_parent().get_node("Camera")
-	player_cam.set_cull_mask(floor_id)
+	if area.get_parent().has_node("Camera"):
+		print(area.name, "Exited")
+		var player_cam = area.get_parent().get_node("Camera")
+		player_cam.set_cull_mask(floor_id)
